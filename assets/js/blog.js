a1afe3f027cc714ec40faf783588ad03c4bb80d8
Adam <aorf@femmeosage.net>
2024-08-09 12:08:47 -0500

    updated html and css for better look. fixed light mode dark mode button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -7,15 +7,23 @@ document.getElementById("back-button").addEventListener("click", function () {
   window.location.href = "index.html";
 });
 
+// updated this function to create seperate containers for each blog post
 function displayPosts() {
   const postsList = document.getElementById("posts-list");
   if (postsList) {
     postsList.innerHTML = "";
     const blogPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
     blogPosts.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p><p><strong>Author:</strong> ${post.username}</p>`;
-      postsList.appendChild(postElement);
+      const postContainer = document.createElement("div");
+      postContainer.className = "post-container";
+      postContainer.innerHTML = `
+                <div class="post">
+                    <h2>${post.title}</h2>
+                    <p>${post.content}</p>
+                    <p><strong>Author:</strong> ${post.username}</p>
+                </div>
+            `;
+      postsList.appendChild(postContainer);
     });
   }
 }
