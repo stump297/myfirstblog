642cfe751e646bf008c8faaf49ad51310524f7fc
Adam <aorf@femmeosage.net>
2024-08-09 11:38:53 -0500

    got the page to work. Having problems with the css

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,22 +1,27 @@
-function displayPosts() {
-  const postsList = document.getElementById("blog");
-  postsList.innerHTML = "";
-  const blogPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
-  blogPosts.forEach((post) => {
-    const postElement = document.createElement("div");
-    postElement.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p><p><strong>Author:</strong> ${post.username}</p>`;
-    postsList.appendChild(postElement);
-  });
-}
-
-window.addEventListener("load", function () {
-  if (document.getElementById("posts-page").style.display === "block") {
-    displayPosts();
-  }
-});
-
-//   adding the light mode/dark mode function
 document.getElementById("toggle-mode").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
   document.body.classList.toggle("light-mode");
+});
+
+document.getElementById("back-button").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+function displayPosts() {
+  const postsList = document.getElementById("posts-list");
+  if (postsList) {
+    postsList.innerHTML = "";
+    const blogPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
+    blogPosts.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p><p><strong>Author:</strong> ${post.username}</p>`;
+      postsList.appendChild(postElement);
+    });
+  }
+}
+
+window.addEventListener("load", function () {
+  if (document.getElementById("posts-page")) {
+    displayPosts();
+  }
 });
